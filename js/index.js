d67cf598408d288be08ebf0766d4a69b9729b4dc
Alexis Romero <romeroalexism@gmail.com>
2018-09-11 13:40:50 -0300

    agregado de nuevas secciones

--- js/index.js
+++ js/index.js
@@ -1,16 +1,25 @@
-$(document).ready(function(){
+$(document).ready(function() {
+  //esconde la web y muestra el preLoader
   $("#web").hide();
-      $("#typed").typed({
-        strings: ["Esto es una prueba de tipeo", "Visible en todos los navegadores", "Parece que anda", "VIVA PERON", "LA PUTA MADRE"],
-        typeSpeed: 30,
-        backDelay: 500,
-        loop: false,
-        loopCount: false,
-        callback: function(){ foo(); }
-      });
-      function foo(){
-        $("#preLoader").hide("drop", { direction: "down" }, "slow" );
-        $("#preLoader").css("display", "none");
-        $("#web").show("fold", 1500 );
-       }
+  $("#typed").typed({
+    strings: ["Esto es una prueba de tipeo"],
+    typeSpeed: 30,
+    backDelay: 500,
+    loop: false,
+    loopCount: false,
+    callback: function() {
+      mostrarWeb();
+    }
+  });
+
+  function mostrarWeb() {
+    $("#preLoader").hide("drop", {
+      direction: "down"
+    }, "slow");
+    $("#preLoader").css("display", "none");
+    $("#web").show("fold", 1500);
+  }
+
+
+
 });
